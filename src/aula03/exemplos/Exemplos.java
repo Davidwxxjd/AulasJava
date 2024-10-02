e715426bbb5da46f36878502746e676d8eade2de
David Souza Santos <david.anglesi.sp@gmail.com>
2024-10-02 09:29:20 -0300

    Comitte dois de outubro, teste de operadores e variaveis

---
+++ src/aula03/exemplos/Exemplos.java
@@ -0,0 +1,22 @@
+package aula03.exemplos;
+
+public class Exemplos {
+    public static void main(String[] args) {
+        int anoNasc;
+        double peso;
+        char sexo;
+        boolean canhoto;
+        float altura;
+        byte idade;
+
+          anoNasc  = 1984;
+          peso = 97;
+          sexo = 'M';
+          altura = 1.8f;
+          idade = 20;
+
+          anoNasc++;
+
+        System.out.println(anoNasc);
+    }
+}
