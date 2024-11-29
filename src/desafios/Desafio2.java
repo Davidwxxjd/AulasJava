138c24e5967ca30999d8dd2f0da30d2cf9d0ac74
unset <unset>
2024-11-29 11:11:11 -0300

    Comite desafio 29/11

---
+++ src/desafios/Desafio2.java
@@ -0,0 +1,17 @@
+package desafios;
+
+import java.util.Scanner;
+
+public class Desafio2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+
+
+
+
+
+        scanner.close();
+    }
+}
